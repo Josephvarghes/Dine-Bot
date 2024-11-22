1dfd649eee82a5d02eedd41234a7dd83d4745098
Joseph Varghese <josephvarghese98128@gmail.com>
2024-11-22 08:45:39 +0530

    completed main.py

--- backend/main.py
+++ backend/main.py
@@ -127,4 +127,16 @@ def remove_from_order(parameters: dict, session_id: str):
 
     return JSONResponse(content={
         "fulfillmentText": fulfillment_text
+    }) 
+
+def track_order(parameters: dict, session_id: str):
+    order_id = int(parameters['order_id'])
+    order_status = db_helper.get_order_status(order_id)
+    if order_status:
+        fulfillment_text = f"The order status for order id: {order_id} is: {order_status}"
+    else:
+        fulfillment_text = f"No order found with order id: {order_id}"
+
+    return JSONResponse(content={
+        "fulfillmentText": fulfillment_text
     })
