6d12a726050a3b28d80effb2ab85bf0b99f9493c
Joseph Varghese <josephvarghese98128@gmail.com>
2024-11-24 13:19:08 +0530

    created db_helper.py

---
+++ backend/db_helper.py
@@ -0,0 +1,44 @@
+# Author: Dhaval Patel. Codebasics YouTube Channel
+
+import mysql.connector
+global cnx
+
+cnx = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="root",
+    database="josephvarghese"
+)
+
+# Function to call the MySQL stored procedure and insert an order item
+def insert_order_item(food_item, quantity, order_id):
+    try:
+        cursor = cnx.cursor()
+
+        # Calling the stored procedure
+        cursor.callproc('insert_order_item', (food_item, quantity, order_id))
+
+        # Committing the changes
+        cnx.commit()
+
+        # Closing the cursor
+        cursor.close()
+
+        print("Order item inserted successfully!")
+
+        return 1
+
+    except mysql.connector.Error as err:
+        print(f"Error inserting order item: {err}")
+
+        # Rollback changes if necessary
+        cnx.rollback()
+
+        return -1
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        # Rollback changes if necessary
+        cnx.rollback()
+
+        return -1
